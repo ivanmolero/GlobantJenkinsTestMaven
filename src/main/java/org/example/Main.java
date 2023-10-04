c53512b328bce8e02b13e7dfbf2e1b1e2e4c703f
ivanmolero <imolerod@gmail.com>
2023-10-04 18:54:49 -0500

    una nueva linea añadida

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("A commit has been made.");
+        System.out.println("Another commit has been made.");
     }
 }
