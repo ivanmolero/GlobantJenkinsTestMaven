c24417786d7629491e9f03bf752ec21da327edd4
ivanmolero <imolerod@gmail.com>
2023-10-04 18:59:46 -0500

    otra prueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("A commit has been made.");
         System.out.println("Another commit has been made.");
+        System.out.println("New test.");
     }
 }
